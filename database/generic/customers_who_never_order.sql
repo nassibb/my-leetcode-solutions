06e1637dbd074cb79a6d4bc9f116542bc9b09194
nassib chaya <nassibc@berkeley.edu>
2026-03-03 21:00:47 -0800

    LeetCode: update database/generic/customers_who_never_order.sql

--- database/generic/customers_who_never_order.sql
+++ database/generic/customers_who_never_order.sql
@@ -3,7 +3,7 @@
 -- Language: SQL (generic)
 -- Link: https://leetcode.com/problems/customers-who-never-order/
 -- Synced by: LinkCode
--- Date: 3/3/2026, 9:00:36 PM
+-- Date: 3/3/2026, 9:00:47 PM
 -- ======================================
 
 
@@ -11,4 +11,4 @@
 SELECT name AS Customers
 FROM Customers AS c LEFT JOIN Orders AS o
 ON c.id = o.customerId
-WHERE o.customerId IS NULL;
+WHERE o.customerId IS NULL

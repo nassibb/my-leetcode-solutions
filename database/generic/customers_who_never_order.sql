38104d2db9befcf08323ebe9506886025f5c18c7
nassib chaya <nassibc@berkeley.edu>
2026-03-03 21:00:37 -0800

    LeetCode: update database/generic/customers_who_never_order.sql

---
+++ database/generic/customers_who_never_order.sql
@@ -0,0 +1,14 @@
+-- ======================================
+-- LeetCode Problem: customers who never order
+-- Language: SQL (generic)
+-- Link: https://leetcode.com/problems/customers-who-never-order/
+-- Synced by: LinkCode
+-- Date: 3/3/2026, 9:00:36 PM
+-- ======================================
+
+
+# Write your MySQL query statement below
+SELECT name AS Customers
+FROM Customers AS c LEFT JOIN Orders AS o
+ON c.id = o.customerId
+WHERE o.customerId IS NULL;

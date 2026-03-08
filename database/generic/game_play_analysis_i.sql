be1585ad64533ffb3b31c98913341d062e1e5fe3
nassib chaya <nassibc@berkeley.edu>
2026-03-07 22:26:13 -0800

    LeetCode: update database/generic/game_play_analysis_i.sql

---
+++ database/generic/game_play_analysis_i.sql
@@ -0,0 +1,13 @@
+-- ======================================
+-- LeetCode Problem: game play analysis i
+-- Language: SQL (generic)
+-- Link: https://leetcode.com/problems/game-play-analysis-i/
+-- Synced by: LinkCode
+-- Date: 3/7/2026, 10:26:12 PM
+-- ======================================
+
+
+# Write your MySQL query statement below
+SELECT player_id, MIN(event_date) AS first_login
+FROM Activity 
+GROUP BY player_id 

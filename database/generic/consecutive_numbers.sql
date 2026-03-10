4a6584f3b943a9fe3551ad2337e8d06056833571
nassib chaya <nassibc@berkeley.edu>
2026-03-10 13:26:57 -0700

    LeetCode: update database/generic/consecutive_numbers.sql

--- database/generic/consecutive_numbers.sql
+++ database/generic/consecutive_numbers.sql
@@ -3,15 +3,15 @@
 -- Language: SQL (generic)
 -- Link: https://leetcode.com/problems/consecutive-numbers/
 -- Synced by: LinkCode
--- Date: 3/10/2026, 1:14:07 PM
+-- Date: 3/10/2026, 1:26:56 PM
 -- ======================================
 
 
 # Write your MySQL query statement below
 SELECT DISTINCT num AS ConsecutiveNums
 FROM (
-SELECT num, a.id - ROW_NUMBER() OVER (PARTITION BY num ORDER BY id) AS label
-FROM Logs a
-) bb
-GROUP BY label, num
+SELECT num, a.id - ROW_NUMBER() OVER (PARTITION BY num ORDER BY id) AS groupy
+FROM Logs AS a
+) AS groupable_values_1_0_num
+GROUP BY groupy, num
 HAVING COUNT(ConsecutiveNums) >= 3

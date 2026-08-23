c28e9521d754c8324220934308ddbdbc131a8293
nassib chaya <nassibc@berkeley.edu>
2026-08-22 22:02:38 -0700

    LeetCode: update python/add_two_numbers.py

---
+++ python/add_two_numbers.py
@@ -0,0 +1,30 @@
+# ======================================
+# LeetCode Problem: add two numbers
+# Language: python
+# Link: https://leetcode.com/problems/add-two-numbers/
+# Synced by: LinkCode
+# Date: 8/22/2026, 10:02:38 PM
+# ======================================
+
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def addTwoNumbers(self, l1, l2):
+        
+        dummy = cur = ListNode(0)
+        carry = 0
+        while l1 or l2 or carry:
+            if l1:
+                carry += l1.val
+                l1 = l1.next
+            if l2:
+                carry += l2.val
+                l2 = l2.next
+            cur.next = ListNode(carry%10)
+            cur = cur.next
+            carry //= 10
+        return dummy.next

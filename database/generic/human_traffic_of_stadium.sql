78158934d89988136b49629d0bfa6107b55cb48f
nassib chaya <nassibc@berkeley.edu>
2026-09-05 11:56:13 -0700

    LeetCode: update database/generic/human_traffic_of_stadium.sql

---
+++ database/generic/human_traffic_of_stadium.sql
@@ -0,0 +1,25 @@
+-- ======================================
+-- LeetCode Problem: human traffic of stadium
+-- Language: SQL (generic)
+-- Link: https://leetcode.com/problems/human-traffic-of-stadium/
+-- Synced by: LinkCode
+-- Date: 9/5/2026, 11:56:13 AM
+-- ======================================
+
+
+# Write your MySQL query statement below
+SELECT id, visit_date, people
+FROM (
+    SELECT *,
+           id - ROW_NUMBER() OVER (ORDER BY id) AS grp
+    FROM Stadium
+    WHERE people >= 100) t
+WHERE grp IN (
+    SELECT grp
+    FROM (
+        SELECT id - ROW_NUMBER() OVER (ORDER BY id) AS grp
+        FROM Stadium
+        WHERE people >= 100) x
+    GROUP BY grp
+    HAVING COUNT(*) >= 3 )
+ORDER BY visit_date;

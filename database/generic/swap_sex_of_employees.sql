82ea74036fc9c7180eceaddf4cbbed0d56022336
nassib chaya <nassibc@berkeley.edu>
2026-09-25 13:35:56 -0700

    LeetCode: update database/generic/swap_sex_of_employees.sql

---
+++ database/generic/swap_sex_of_employees.sql
@@ -0,0 +1,17 @@
+-- ======================================
+-- LeetCode Problem: swap sex of employees
+-- Language: SQL (generic)
+-- Link: https://leetcode.com/problems/swap-sex-of-employees/
+-- Synced by: LinkCode
+-- Date: 9/25/2026, 1:35:55 PM
+-- ======================================
+
+
+# Write your MySQL query statement below
+UPDATE salary 
+SET sex =
+(CASE 
+WHEN sex = 'f' THEN 'm'
+ELSE 'f'
+END)
+
